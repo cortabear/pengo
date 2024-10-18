4c8b97980a7cd9a835b4a27812880ba307001a1b
Elwood Berry <github@elwoodberry.com>
2024-10-18 03:22:59 -0500

    Updated indicator

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Cortabox
-Our signature Pine Script that paints a box when the GAP meets specific requirements.
+# Corta.Box
+
--- _legacy/levels.py
+++
@@ -1,8 +0,0 @@
-//@version=5
-indicator("Line 16 Ticks Above Current Candle", overlay=true)
-
-// Calculate the price 16 ticks above the current candle's close
-price_16_ticks_above = close + syminfo.mintick * 16
-
-// Plot the line
-line.new(x1 = bar_index, y1 = close, x2 = bar_index, y2 = price_16_ticks_above, color = color.blue, width = 2, extend = extend.both)
